12205991e545ec574a8f1a1ebc7cd6aa23d17060
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-13 21:42:41 -0500

    When user presses sign in button, model window displays

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.querySelector('.signin');
+  var modal = document.querySelector('.modal');
+
+  button.addEventListener('click', function() {
+    modal.style.display = "unset";
+  })
+
+
+
+});
