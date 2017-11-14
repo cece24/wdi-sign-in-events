96d128416a04fbdf266c8a1e224b76375ffb3627
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-13 21:45:33 -0500

    When user presses close button, hide modal window

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   var button = document.querySelector('.signin');
   var modal = document.querySelector('.modal');
+  var close = document.querySelector('.close');
 
   button.addEventListener('click', function() {
     modal.style.display = "unset";
+
+    close.addEventListener('click', function() {
+      modal.style.display = "none";
+    })
   })
 
 
