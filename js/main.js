1d9cf5455c74733ba010fe3a83acc8af0a9a44b9
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-13 21:57:27 -0500

    When user presses submit button, add .error class to input elements

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
   var button = document.querySelector('.signin');
   var modal = document.querySelector('.modal');
   var close = document.querySelector('.close');
+  var inputUser = document.querySelector('#user');
+  var inputPass = document.querySelector('#pass');
+  var submitButton = document.querySelector('.submit');
 
   button.addEventListener('click', function() {
     modal.style.display = "unset";
 
+    submitButton.addEventListener('click', function() {
+      inputUser.classList.add('error');
+      inputPass.classList.add('error');
+    })
+
     close.addEventListener('click', function() {
       modal.style.display = "none";
     })
+
+
   })
 
 
